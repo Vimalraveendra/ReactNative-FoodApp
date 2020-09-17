4dad881a8dedd8928cc3eabc816ed01cb3406845
vimal Raveendran <vimalraveendra@gmail.com>
2020-09-17 15:03:56 +0200

    Created NavigatorFolder & added StackScreen, also created Components folder and added HomeScreen component

--- App.js
+++ App.js
@@ -1,25 +1,9 @@
 import React from 'react';
 
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
+import StackScreen from './Navigators/StackScreen/StackScreen';
 
 const App = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Food App</Text>
-    </SafeAreaView>
-  );
+  return <StackScreen />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 23,
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ Components/Home/HomeScreen.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen component</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+export default HomeScreen;
