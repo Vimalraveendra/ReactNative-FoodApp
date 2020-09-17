28a4598b9bec932739d8e20e0c751f7402deafb3
vimal Raveendran <vimalraveendra@gmail.com>
2020-09-17 16:00:57 +0200

    added react-native-scrollable-tab-vies, Updated HomeScreen component to render the component based on the scroll view & also created assets folder & added images

--- Components/Home/HomeScreen.js
+++ Components/Home/HomeScreen.js
@@ -1,10 +1,31 @@
 import * as React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image, Dimensions} from 'react-native';
+import ScrollableTabView, {
+  DefaultTabBar,
+} from 'react-native-scrollable-tab-view';
+const {width} = Dimensions.get('screen');
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Home Screen component</Text>
+      <View style={styles.header}>
+        <Image
+          style={styles.image}
+          source={require('../assets/images/food3.jpg')}
+        />
+        <Text style={styles.title}>Food App</Text>
+      </View>
+      <View style={styles.tabBar}>
+        <ScrollableTabView
+          style={styles.tabView}
+          initialPage={0}
+          tabBarActiveTextColor="blue"
+          renderTabBar={() => <DefaultTabBar />}>
+          <Text tabLabel="Main">Main</Text>
+          <Text tabLabel="Menu">Menu</Text>
+          <Text tabLabel="Popular">Popular</Text>
+        </ScrollableTabView>
+      </View>
     </View>
   );
 };
@@ -12,8 +33,30 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  header: {
+    marginTop: 0,
+  },
+  title: {
+    position: 'absolute',
+    fontSize: 30,
+    fontWeight: '700',
+    alignSelf: 'center',
+    color: 'blue',
+  },
+  image: {
+    width: width,
+    height: width / 2,
+    resizeMode: 'cover',
+  },
+
+  tabBar: {
+    flex: 1,
+    paddingHorizontal: 30,
+  },
+  tabView: {
+    marginTop: 20,
   },
 });
 export default HomeScreen;
