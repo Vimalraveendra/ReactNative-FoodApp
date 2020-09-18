e4a5c050c035497d2c4a7929da6e08848ac209b5
vimal Raveendran <vimalraveendra@gmail.com>
2020-09-18 15:24:49 +0200

    Added Main component to rendered the main food items & also added react-native-vector-icons to render appropriate icons

---
+++ Components/Main/Main.js
@@ -0,0 +1,173 @@
+import * as React from 'react';
+import {
+  View,
+  Image,
+  StyleSheet,
+  FlatList,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+
+import LinearGradient from 'react-native-linear-gradient';
+import Icon from 'react-native-vector-icons/Ionicons';
+import AntIcon from 'react-native-vector-icons/AntDesign';
+Icon.loadFont();
+AntIcon.loadFont();
+
+const data = [
+  {
+    name: 'Stewed Mushrooms',
+    image: require('../assets/images/food1.jpg'),
+    rating: 3,
+    price: '$12',
+  },
+  {
+    name: 'Jackfruit Fried',
+    image: require('../assets/images/food2.jpg'),
+    rating: 5,
+    price: '$15',
+  },
+  {
+    name: 'Noodles',
+    image: require('../assets/images/food3.jpg'),
+    rating: 4,
+    price: '$20',
+  },
+  {
+    name: 'Beef',
+    image: require('../assets/images/food4.jpg'),
+    rating: 2,
+    price: '$12',
+  },
+  {
+    name: 'Salad dressing',
+    image: require('../assets/images/food5.jpg'),
+    rating: 5,
+    price: '$13',
+  },
+];
+
+class Main extends React.Component {
+  state = {
+    data: data,
+  };
+
+  ratingStar = (rating) => {
+    let stars = [];
+    //Loop 5 times
+    for (let i = 1; i <= 5; i++) {
+      // setting rating to filled star
+      let name = 'ios-star';
+
+      // If ratings is lower, set the rating to unfilled star
+      if (i > rating) {
+        name = 'ios-star-outline';
+      }
+      stars.push(<Icon name={name} size={15} style={styles.star} key={i} />);
+    }
+
+    return stars;
+  };
+
+  renderItem = ({item}) => {
+    return (
+      <LinearGradient
+        colors={['#4c669f', '#3b5998']}
+        start={{x: 0, y: 1}}
+        end={{x: 1, y: 0}}
+        style={styles.item}>
+        <View style={styles.imageContainer}>
+          <Image source={item.image} style={styles.image} />
+        </View>
+        <View style={styles.nameContent}>
+          <Text style={styles.name}>{item.name}</Text>
+          <View style={styles.rating}>{this.ratingStar(item.rating)}</View>
+          <View style={styles.priceContainer}>
+            <Text style={styles.price}>{item.price}</Text>
+          </View>
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <AntIcon name="arrowright" size={20} color="blue" />
+        </TouchableOpacity>
+      </LinearGradient>
+    );
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.list}>
+          <FlatList
+            data={this.state.data}
+            renderItem={this.renderItem}
+            keyExtractor={(item, index) => index.toString()}
+            showsVerticalScrollIndicator={false}
+          />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  list: {
+    flex: 1,
+  },
+  item: {
+    flex: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    borderRadius: 10,
+    marginVertical: 8,
+  },
+  imageContainer: {
+    width: 90,
+    height: 90,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    borderWidth: 5,
+    borderColor: '#fff',
+    borderRadius: 10,
+  },
+  nameContent: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: '#fff',
+  },
+  rating: {
+    flexDirection: 'row',
+    marginTop: 5,
+  },
+  star: {
+    color: '#FF8C00',
+  },
+  button: {
+    width: 30,
+    height: 30,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+  },
+  priceContainer: {
+    marginTop: 10,
+  },
+  price: {
+    color: '#fff',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+});
+export default Main;
