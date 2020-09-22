2f7e7678ce8ed409f6e080413d4f28eba0bbecbf
vimal Raveendran <vimalraveendra@gmail.com>
2020-09-22 13:11:35 +0200

    updated Deatiails component to render the specify details of the item

--- Components/Details/Details.js
+++ Components/Details/Details.js
@@ -1,20 +1,50 @@
 import React from 'react';
 
-import {View, Image, StyleSheet, Dimensions} from 'react-native';
+import {
+  View,
+  Image,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  Text,
+  ScrollView,
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+import AntIcon from 'react-native-vector-icons/AntDesign';
+AntIcon.loadFont();
 
 const {width, height} = Dimensions.get('screen');
-console.log('hi', height);
 
 const Details = (props) => {
-  console.log('item', props.route.params);
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <View style={styles.imageContainer}>
           <Image source={props.route.params.image} style={styles.image} />
         </View>
+        <View style={styles.backArrow}>
+          <TouchableOpacity onPress={() => props.navigation.goBack()}>
+            <AntIcon name="arrowleft" size={30} color="#fff" />
+          </TouchableOpacity>
+        </View>
       </View>
-      <View style={styles.footer}></View>
+      <ScrollView style={styles.footer}>
+        <View style={styles.status}>
+          <Text style={styles.statusText}>AVAILABLE</Text>
+        </View>
+        <Text style={styles.name} numberOfLines={1}>
+          {props.route.params.name}
+        </Text>
+        <Text style={styles.price}>{props.route.params.price}</Text>
+        <LinearGradient
+          start={{x: 0, y: 0}}
+          end={{x: 1, y: 0}}
+          colors={['#333CFF', '#33A2FF']}
+          style={styles.gradient}>
+          <Text style={styles.buttonText}>order N ow</Text>
+        </LinearGradient>
+      </ScrollView>
     </View>
   );
 };
@@ -33,7 +63,7 @@ const styles = StyleSheet.create({
     width: width / 3,
     height: height / 6,
     alignSelf: 'center',
-    marginTop: height / 14,
+    marginTop: height / 12,
   },
   image: {
     width: '100%',
@@ -42,9 +72,55 @@ const styles = StyleSheet.create({
     borderColor: '#fff',
     borderRadius: (height / width) * 20,
   },
+  backArrow: {
+    position: 'absolute',
+    left: 0,
+    marginTop: 30,
+    marginLeft: 15,
+  },
   footer: {
     flex: 2,
     backgroundColor: '#fff',
+    paddingHorizontal: 50,
+    paddingVertical: 60,
+  },
+  status: {
+    width: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 50,
+    paddingVertical: 5,
+    borderColor: '#3349FF',
+  },
+  statusText: {
+    color: '#13D313',
+  },
+  name: {
+    color: '#3e3c3e',
+    fontWeight: '500',
+    fontSize: 27,
+    marginTop: 20,
+    textTransform: 'uppercase',
+  },
+  price: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  gradient: {
+    borderRadius: 50,
+  },
+  buttonText: {
+    fontSize: 22,
+    fontFamily: 'Gill Sans',
+    textAlign: 'center',
+    margin: 10,
+    color: '#ffffff',
+    backgroundColor: 'transparent',
+    textTransform: 'uppercase',
   },
 });
 
